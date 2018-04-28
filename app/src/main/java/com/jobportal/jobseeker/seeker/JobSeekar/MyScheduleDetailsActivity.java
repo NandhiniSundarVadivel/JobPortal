20c5959f5a8537dbbfe2f53a9e0bd7c050920f4d
Akila <akil.uniflyn@gmail.com>
2018-04-28 18:44:31 +0530

    "Home Screen Changes"

---
+++ app/src/main/java/com/jobportal/jobseeker/seeker/JobSeekar/MyScheduleDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.jobportal.jobseeker.seeker.JobSeekar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jobportal.jobseeker.seeker.R;
+
+public class MyScheduleDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_schedule_details);
+    }
+}
